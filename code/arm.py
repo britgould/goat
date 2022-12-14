85854fa8c9096941b8ac0fad84ebfc0398ed9e3a
Brit Gould <brit@britgould.com>
2022-12-13 22:33:56 -0800

    First partially-functional inverse kinematics code

--- code/arm.py
+++ code/arm.py
@@ -22,42 +22,39 @@ def initialize():
     global my_chain
     my_chain = ikpy.chain.Chain.from_urdf_file("goat_arm.urdf", active_links_mask=[False, True, True, True, True, True])
 
-    logging.info('arm initialized.')
-    return True
-
-
-def moveTest():
     global target_position
     global target_orientation
-    target_position = [0, 0, 0.58]
-    target_orientation = [-1, 0, 0]
+    target_position = [0, 0, 63]
+    target_orientation = [0, 1, 0]
 
-    global ik
-    ik = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Y")
-    print("The angles of each joints are : ", list(map(lambda r:math.degrees(r),ik.tolist())))
+    global inverseKinematics
+    inverseKinematics = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Y")
+    print("The angles of each joints are : ", list(map(lambda r:math.degrees(r),inverseKinematics.tolist())))
 
-    computed_position = my_chain.forward_kinematics(ik)
+    computed_position = my_chain.forward_kinematics(inverseKinematics)
     print("Computed position: %s, original position : %s" % (computed_position[:3, 3], target_position))
     print("Computed position (readable) : %s" % [ '%.2f' % elem for elem in computed_position[:3, 3] ])
 
     global fig
     global ax
     fig, ax = plot_utils.init_3d_figure()
-    fig.set_figheight(9)
-    fig.set_figwidth(13)
-    my_chain.plot(ik, ax, target=target_position)
-    plt.xlim(-0.5, 0.5)
-    plt.ylim(-0.5, 0.5)
-    ax.set_zlim(0, 0.6)
+#    fig.set_figheight(9)
+#    fig.set_figwidth(13)
+    my_chain.plot(inverseKinematics, ax, target=target_position)
+    plt.xlim(-65, 65)
+    plt.ylim(-65, 65)
+    ax.set_zlim(0, 65)
     plt.ion()
 
+    logging.info('arm initialized.')
+    return True
 
-def doIK():
-    global ik
+def doInverseKinematics():
+    global inverseKinematics
     global target_position
     global target_orientation
-    old_position = ik.copy()
-    ik = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Z",
+    old_position = inverseKinematics.copy()
+    inverseKinematics = my_chain.inverse_kinematics(target_position, target_orientation, orientation_mode="Z",
                                      initial_position=old_position)
 
 
@@ -66,27 +63,30 @@ def updatePlot():
     global ax
     
     ax.clear()
-    my_chain.plot(ik, ax, target=target_position)
-    plt.xlim(-0.5, 0.5)
-    plt.ylim(-0.5, 0.5)
-    ax.set_zlim(0, 0.6)
+    my_chain.plot(inverseKinematics, ax, target=target_position)
+    plt.xlim(-65, 65)
+    plt.ylim(-65, 65)
+    ax.set_zlim(0, 65)
     fig.canvas.draw()
     fig.canvas.flush_events()
-
 
 def move(x, y, z):
     global target_position
     target_position = [x, y, z]
-    doIK()
+    doInverseKinematics()
     updatePlot()
-
-#initialize()
-#moveTest()
-
-#move(0,0.2,0.3)
-
 
 def shutdown():
     logging.debug('arm shutting down...')
 
     logging.info('arm shut down complete.')
+
+initialize()
+
+print('one')
+#input('proceed?')
+
+move(0,63,13)
+
+#input('proceed?')
+print('two')
